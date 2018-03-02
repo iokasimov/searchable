375576dc9f3e3ae2bfa79e18b4031c9c47ed58aa
Murat Kasimov <iokasimov.m@gmail.com>
2018-03-02 12:04:16 +0300

    Add license, cabal and stack configs, update README.md

---
+++ src/Data/Searchable.hs
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Searchable
+-- Copyright   :  (c) Murat Kasimov 2018
+-- License     :  BSD3
+--
+-- Maintainer  :  Murat Kasimov <iokasimov.m@gmail.com>
+-- Stability   :  provisional
+-- Portability :  non-portable
+--
+-----------------------------------------------------------------------------
+
+module Data.Searchable () where
