8ccaaf06b3fbdac91159b1178b11e363d63b0134
Murat Kasimov <iokasimov.m@gmail.com>
2018-03-02 15:24:48 +0300

    Create class and several instances

--- src/Data/Searchable.hs
+++ src/Data/Searchable.hs
@@ -10,4 +10,25 @@
 --
 -----------------------------------------------------------------------------
 
-module Data.Searchable () where
+module Data.Searchable (Searchable (..)) where
+
+import Control.Applicative
+import Data.Functor.Identity
+
+class (Functor t, Foldable t) => Searchable t where
+    search :: Alternative f => (a -> f b) -> t a -> f (t b)
+
+instance Searchable (Const r) where
+    search _ (Const r) = pure $ Const r
+
+instance Searchable Identity where
+    search f (Identity x) = Identity <$> f x
+
+instance Searchable Maybe where
+    search _ Nothing  = pure Nothing
+    search f (Just x) = Just <$> f x
+
+instance Searchable [] where
+    search _ []     = empty
+    search f (x:xs) = ((:) <$> f x <*> search f xs)
+        <|> search f xs <|> pure []
